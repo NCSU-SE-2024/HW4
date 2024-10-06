0780e8dd47225792b139cdc06c310645204f0072
Gaurav Sheth <gauravsheth2001@gmail.com>
2024-10-06 15:16:50 -0400

    Added task1.sh

---
+++ task1.sh
@@ -0,0 +1,7 @@
+pid=$(ps aux | grep "[b]ash infinite.sh" | awk '{print $2}')
+if [[ -z "$pid" ]]; then
+  echo "Process infinite.sh not running."
+else
+  kill -9 $pid
+  echo "Process infinite.sh with pid: $pid is killed."
+fi

1ddb911e4031cbf49ea7e4183f35e6fc85ebf176
Gaurav Sheth <80212233+gsheth21@users.noreply.github.com>
2024-10-23 18:26:41 -0400

    Update task2.sh
    modified task2 to sort based on file size in tie breaker in sorting

--- task2.sh
+++ task2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd dataset1/
-printf "%s\n" $(grep -rl "sample" file* | xargs grep -c "CSC510" | grep -E ":[3-9][0-9]*$" |\
-sort -t: -k2,2nr | cut -d: -f1 | uniq |\
-sed 's/file_/filtered_/' | gawk '{ print $0}')
+printf "%s\n" $(grep -rl "sample" file* | \
+ xargs -I {} sh -c 'count=$(grep -c "CSC510" "{}"); size=$(stat -c%s "{}"); if [ "$count" -ge 3 ]; then echo "{} $count $size"; fi' | \
+ sort -k2,2nr -k3,3nr | \
+ gawk '{gsub(/file_/, "filtered_", $1); print $1}')
